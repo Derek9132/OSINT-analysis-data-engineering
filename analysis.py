77cd210e8b613e3f70e6431f01b193d054c3a9ec
Derek9132 <derek9132@gmail.com>
2026-01-12 18:32:51 -0500

    project setup

---
+++ analysis.py
@@ -0,0 +1,21 @@
+import re
+import nltk
+import requests
+import mitreattack.attackToExcel.stixToDf as stixToDf
+import pandas as pd
+
+def json_to_df():
+    pass
+
+def csv_to_df(link):
+    pass
+
+
+if __name__ == "main":
+    # read in MITRE ATT&CK STIX
+
+    # read in CISA advisories
+
+    # read in NVD CVE
+
+    pass
