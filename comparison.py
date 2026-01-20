4253b44a11353261d517d1d5442efb75d48d4e56
Derek9132 <derek9132@gmail.com>
2026-01-20 14:51:10 -0500

    updated comparison.py

--- comparison.py
+++ comparison.py
@@ -10,6 +10,12 @@ def to_sql_table(dataframe, table_name, engine):
 
     dataframe.to_sql(name=table_name, con=engine, if_exists="replace", index=False)
 
+def split_technique(technique):
+    if "." in technique:
+        return technique.split(".")[0]
+    else:
+        return technique
+
 if __name__ == "__main__":
     try:
         base_dir = Path(__file__).resolve().parent
@@ -17,24 +23,36 @@ if __name__ == "__main__":
         # enterprise-attack dataframes
         enterprise_path = base_dir/"raw"/"enterprise-stix.xlsx"
         enterprise_techniques = pd.read_excel(enterprise_path, sheet_name=0)
-        enterprise_examples = pd.read_excel(enterprise_path, sheet_name=1)
-        enterprise_mitigations = pd.read_excel(enterprise_path, sheet_name=2)
-        enterprise_detections = pd.read_excel(enterprise_path, sheet_name=3)
 
         # mobile-attack dataframes
         mobile_path = base_dir/"raw"/"mobile-stix.xlsx"
         mobile_techniques = pd.read_excel(mobile_path, sheet_name=0)
-        mobile_examples = pd.read_excel(mobile_path, sheet_name=1)
-        mobile_mitigations = pd.read_excel(mobile_path, sheet_name=2)
-        mobile_detections = pd.read_excel(mobile_path, sheet_name=3)
 
-        enterprise_not_mobile = enterprise_techniques[~enterprise_techniques["ID"].isin(mobile_techniques["ID"])]
+        # dataframes
+        enterprise_subset = pd.DataFrame(columns=["technique", "technique name", "description", "domain"])
+        mobile_subset = pd.DataFrame(columns=["technique", "technique name", "description", "domain"])
+
+        enterprise_subset["technique"] = enterprise_techniques["ID"]
+        enterprise_subset["technique name"] = enterprise_techniques["name"]
+        enterprise_subset["description"] = enterprise_techniques["description"]
+        enterprise_subset["domain"] = enterprise_techniques["domain"]
+
+        mobile_subset["technique"] = mobile_techniques["ID"]
+        mobile_subset["technique name"] = mobile_techniques["name"]
+        mobile_subset["description"] = mobile_techniques["description"]
+        mobile_subset["domain"] = mobile_techniques["domain"]
+
+        # make new columns
+        enterprise_subset["parent technique"] = enterprise_subset["technique"].map(split_technique)
+        mobile_subset["parent technique"] = mobile_subset["technique"].map(split_technique)
+
+        enterprise_not_mobile = enterprise_subset[~enterprise_subset["parent technique"].isin(mobile_subset["parent technique"])]
 
         # find techniques in mobile-attack not in enterprise-attack
-        mobile_not_enterprise = mobile_techniques[~mobile_techniques["ID"].isin(enterprise_techniques["ID"])]
+        mobile_not_enterprise = mobile_subset[~mobile_subset["parent technique"].isin(enterprise_subset["parent technique"])]
 
         # find common techniques
-        enterprise_and_mobile = enterprise_techniques[enterprise_techniques["ID"].isin(mobile_techniques["ID"])]
+        enterprise_and_mobile = enterprise_subset[enterprise_subset["parent technique"].isin(mobile_subset["parent technique"])]
 
         # export
         with pd.ExcelWriter(base_dir/"comparison"/"comparison.xlsx") as writer:
